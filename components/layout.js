cf34e58bd3818803088e4f063b206c61ce6e5c0e
Jeff Walsh <jeffpwalsh@gmail.com>
2020-06-02 13:06:20 +0200

    google analytics added

--- components/layout.js
+++ components/layout.js
@@ -1,23 +1,35 @@
-import Navbar from "./navbar";
-import Head from "next/head";
+import Navbar from './navbar';
+import Head from 'next/head';
+import { initGA, logPageView } from '../utils/googleAnalytics';
 
-//COMPONENT WRAPS LAYOUT OVER PAGES
-const Layout = (props) => (
-  <div>
-    <Head>
-      <title>portfolio</title>
-      <link
-        rel="stylesheet"
-        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-        crossorigin="anonymous"
-      ></link>
-    </Head>
-    <Navbar />
-    {/* PASSES STYLING DOWN TO CHILDREN */}
-    {props.children}
-    {/* LOCAL STYLED JSX */}
-    <style global jsx>{`
+export default class Layout extends React.Component {
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }
+  render() {
+    return (
+      //COMPONENT WRAPS LAYOUT OVER PAGES
+
+      <div>
+        <Head>
+          <title>portfolio</title>
+
+          <link
+            rel='stylesheet'
+            href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+            integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T'
+            crossOrigin='anonymous'
+          ></link>
+        </Head>
+        <Navbar />
+        {/* PASSES STYLING DOWN TO CHILDREN */}
+        {this.props.children}
+        {/* LOCAL STYLED JSX */}
+        <style global jsx>{`
       body {
         background: url("../static/images/Image1.jpg") no-repeat;
         background-attachment: fixed;
@@ -25,7 +37,7 @@ const Layout = (props) => (
       }, 
       
     `}</style>
-  </div>
-);
-
-export default Layout;
+      </div>
+    );
+  }
+}
