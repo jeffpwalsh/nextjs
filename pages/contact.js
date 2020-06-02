4390926bd8949a798bb53759a2bbda9ff8a88d9f
Jeff Walsh <jeffpwalsh@gmail.com>
2020-06-02 13:29:30 +0200

    added L3 capstone to projects

--- pages/contact.js
+++ pages/contact.js
@@ -1,57 +1,67 @@
-import Layout from "../components/layout";
+import Layout from '../components/layout';
 
 //LAYOUT WRAPPED COMPONENT
 const Contact = () => (
   <Layout>
-    <div className="about">
+    <div className='about'>
       <h2>Contact</h2>
       <img
-        className="icon"
-        id="icon"
-        src="../static/images/icons8-phone-24.png"
+        className='icon'
+        id='icon'
+        src='../static/images/icons8-phone-24.png'
       />
       <br />
-      <button type="button" className="btn btn-info">
-        Tel <span className="badge badge-light"> 072-157-0009 </span>
+      <button type='button' className='btn btn-info'>
+        Tel <span className='badge badge-light'> 072-157-0009 </span>
       </button>
 
-      <button type="button" className="btn btn-info">
-        Email <span className="badge badge-light"> jeffpwalsh@gmail.com </span>
+      <button type='button' className='btn btn-info'>
+        Email <span className='badge badge-light'> jeffpwalsh@gmail.com </span>
       </button>
 
-      <button type="button" className="btn btn-info">
-        Address <span className="badge badge-light"> Blouberg, Cape Town </span>
+      <button type='button' className='btn btn-info'>
+        Address <span className='badge badge-light'> Blouberg, Cape Town </span>
       </button>
 
-      <button type="button" className="btn btn-info">
-        Linkedin{" "}
-        <span className="badge badge-light"> jeff-walsh-1445414a </span>
-      </button>
+      <a href='https://www.linkedin.com/in/jeff-walsh-1445414a/' target="_blank">
+        <button type='button' className='btn btn-primary'>
+          Linkedin{' '}
+          <span className='badge badge-light'> jeff-walsh-1445414a </span>
+        </button>
+      </a>
+      <a href='https://github.com/jeffpwalsh' target="_blank">
+        <button type='button' className='btn btn-warning'>
+          GitHub{' '}
+          <span className='badge badge-light'> https://github.com/jeffpwalsh </span>
+        </button>
+      </a>
     </div>
     {/* LOCAL STYLED JSX */}
-    <style jsx>{`
-      .about {
-        margin: 10% 0%;
-        border: 1px solid;
-        text-align: center;
-        width: 75%;
-        background-color: #f3f3f3;
-        margin-left: auto;
-        margin-right: auto;
-        padding: 30px;
-      }
-      ,
-      .btn {
-        margin: 10px;
-      }
-    `}
+    <style jsx>
+      {`
+        .about {
+          margin: 10% 0%;
+          border: 1px solid;
+          text-align: center;
+          width: 75%;
+          background-color: #f3f3f3;
+          margin-left: auto;
+          margin-right: auto;
+          padding: 30px;
+        }
+        ,
+        .btn {
+          margin: 10px;
+        }
+      `}
     </style>
     {/* GLOBAL STYLED JSX */}
-    <style global jsx>{`
-      .icon {
-        padding: 20px 0px;
-      }     
-    `}
+    <style global jsx>
+      {`
+        .icon {
+          padding: 20px 0px;
+        }
+      `}
     </style>
   </Layout>
 );
--- pages/projects.js
+++ pages/projects.js
@@ -1,45 +1,60 @@
-import Layout from "../components/layout";
+import Layout from '../components/layout';
 
 //LAYOUT WRAPPED COMPONENT
 const Projects = () => (
   <Layout>
-    <div className="about">
+    <div className='about'>
       <h2>Projects</h2>
       <img
-        className="icon"
-        src="../static/images/icons8-project-diagram-32.png"
+        className='icon'
+        src='../static/images/icons8-project-diagram-32.png'
       />
-      <table className="table table-dark table-hover">
+      <table className='table table-dark table-hover'>
         <thead>
           <tr>
             <th>Name</th>
             <th>Description</th>
             <th>Link</th>
+            <th>Tech Stack</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Hangman</td>
-            <td>React - Hangman game with "car manufacturers"</td>
-            <td>
-              <a href="https://jeffpwalsh-hangman.herokuapp.com/">deployed</a>
-            </td>
-          </tr>
+         
           <tr>
             <td>iTunes Search API</td>
-            <td>React, Node, Express - Search iTunes and save favourites</td>
+            <td>Search iTunes and save favourites</td>
             <td>
-              <a href="https://guarded-tor-84227.herokuapp.com/">deployed</a>
+              <a href='https://guarded-tor-84227.herokuapp.com/'>deployed</a>
             </td>
+            <td>React, Node, Express </td>
           </tr>
           <tr>
             <td>Weather API</td>
-            <td>React - Check the current weather for different cities.</td>
+            <td>Check the current weather for different cities.</td>
             <td>
-              <a href="https://jeffpwalsh-weather-finder.herokuapp.com/">
+              <a href='https://jeffpwalsh-weather-finder.herokuapp.com/'>
                 deployed
               </a>
             </td>
+            <td>React</td>
+          </tr>
+          <tr>
+            <td>Animal Adopter</td>
+            <td>Animal adoption social platform</td>
+            <td>
+              <a href='https://animal-adopter.herokuapp.com/'>deployed</a>
+            </td>
+            <td>MongoDB, Express, Node, React</td>
+          </tr>
+          <tr>
+            <td>Hangman</td>
+            <td>Hangman game with "car manufacturers"</td>
+            <td>
+              <a href='https://jeffpwalsh-hangman.herokuapp.com/'>
+                temporarily removed
+              </a>
+            </td>
+            <td>React</td>
           </tr>
         </tbody>
       </table>
